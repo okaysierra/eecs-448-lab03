9b17466b33647599092ba31a19602b1225d7caa4
okaysierra <scseacat@gmail.com>
2016-02-18 18:41:01 -0600

    Fix miscellaneous bugs causing program not work.

--- validate.js
+++ validate.js
@@ -1,13 +1,13 @@
 function validate()
 {
-  var a = document.getElementById("a");
-  var b = document.getElementById("b");
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
 
-  if(a != b)
+  if(b !== a)
     {
       alert("The entered passwords don't match. please reenter the passwords");
     }
-  else if(a.length()>8)
+  else if(a.length<8)
     {
       alert("The password is not long enough. The password needs to be at least 8 characters long");
     }
