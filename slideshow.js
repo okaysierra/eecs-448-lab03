72f557d953d9f6a6c21d7195a6e5a95853e70091
okaysierra <scseacat@gmail.com>
2016-02-20 16:22:16 -0600

    Butttons control image changes.

---
+++ slideshow.js
@@ -0,0 +1,37 @@
+var images = ["resources/marie_curie.png" , "resources/python.png", "resources/sphere.png",
+              "resources/tree.png", "resources/unscientific.png"];
+var i =0;
+
+document.write("<img src="+images[i]+" width=500px >");
+
+function next()
+{
+  if(i==images.length-1)
+  {
+    i=0;
+
+  }
+  else
+  {
+    i++;
+
+  }
+
+  document.write("<img src ="+images[i]+" width=500px>");
+
+}
+
+function previous()
+{
+  if(i==0)
+    {
+      i=images.length-1;
+
+    }
+  else
+    {
+      i--;
+
+    }
+    document.write("<img src ="+images[i]+" width=500px>");
+}
